8d7e682eb1d573eb5f562e82f38a0399aea45280
Manoj <Manoj>
2020-10-14 19:54:32 -0400

    Server started listening at 127.0.0.1.3009 and implemented data storage

--- index.js
+++ index.js
@@ -5,3 +5,18 @@
     Date : 14.10.2020
     Assignment1
 */
+
+var SERVER_NAME = 'product-api'
+var PORT = 3009;
+var HOST = '127.0.0.1';
+
+
+var restify = require('restify')
+  , productData = require('save')('products')
+  , server = restify.createServer({ name: SERVER_NAME})
+
+  server.listen(PORT, HOST, function () {
+  console.log('Server %s listening at %s', server.name, server.url)
+  console.log(' /products')
+  
+})
