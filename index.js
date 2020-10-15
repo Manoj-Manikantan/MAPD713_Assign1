5ba094489c59ec1c0c19c69c7c096ba9f3294d0a
Manoj <Manoj>
2020-10-14 20:30:00 -0400

    log requests added

--- index.js
+++ index.js
@@ -17,21 +17,28 @@ var restify = require('restify')
 
 server.listen(PORT, HOST, function () {
     console.log('Server %s listening at %s', server.name, server.url, '/products')
+    console.log("Endpoints:")
+    console.log("http://127.0.0.1:3009/products method: GET, POST, DELETE")
+    console.log("GET method to display all products")
+    console.log("POST method to add a new product information")
+    console.log("DELETE method to delete all products")
 })
 
 server
     .use(restify.fullResponse())
     .use(restify.bodyParser())
-
 /* To get all products */
 server.get('/products', function (req, res, next) {
+    console.log("products GET: received request");
     productData.find({}, function (error, products) {
         res.send(products)
+        console.log("products GET: sending response");
     })
 })
 
-/* To add a product */
+/* To add a product information */
 server.post('/products', function (req, res, next) {
+    console.log("products POST: received request");
     if (req.params.name === undefined) {
         return next(new restify.InvalidArgumentError('name must be supplied'))
     }
@@ -49,13 +56,16 @@ server.post('/products', function (req, res, next) {
     productData.create(newProduct, function (error, product) {
         if (error) return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
         res.send(201, product)
+        console.log("products POST: sending response");
     })
 })
 
 /* To delete all products */
 server.del('/products', function (req, res, next) {
+    console.log("products DELETE: received request");
     productData.deleteMany({}, function (error, product) {
         if (error) return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
         res.send()
+        console.log("products DELETE: sending response");
     })
 })
